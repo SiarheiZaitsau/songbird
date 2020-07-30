f5dd80ed57b4fc7725bba65606f3e8ba1b514560
SiarheiZaitsau <zaitsevser364@gmail.com>
2020-07-30 13:43:47 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">123</div>;
+  return <div className="App">123dasdas</div>;
 }
 
 export default App;
